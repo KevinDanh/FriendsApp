e6e7b16a4822616cd9e85a58bc3b798ce32f884e
jshal001 <jihadshalabi18@gmail.com>
2022-07-12 13:45:21 -0700

    Change hello world to shalabi world

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 function App() {
-	return <div className="App">
-    <h1>HelloWorld!</h1>
-  </div>;
+	return (
+		<div className="App">
+			<h1>shalabi world!</h1>
+		</div>
+	);
 }
 
 export default App;
