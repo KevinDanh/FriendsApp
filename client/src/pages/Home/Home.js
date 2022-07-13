004f737dfb767d6fb3638750664527ca4b747168
BrianSaldana <brianxsaldana@gmail.com>
2022-07-12 21:47:01 -0700

    Home Page UI Complete and Running

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import "./home.css";
+
 function Home() {
 	return (
-		<div>
-			<Button variant="contained" color="secondary">
-				Login
-			</Button>
-			<Button variant="contained">Register</Button>
+		<div className="brownContainer">
+			<div className="orangeContainer">
+				<div className ="whiteContainer">
+					<h1 className = "titleContainer1">Friends</h1>
+					<h1 className = "titleContainer2">Friends</h1>
+					<div className ="sloganContainer1">
+						<p>Plan.</p>
+						<p>Schedule.</p>
+						<p>HangOut.</p>
+					</div>
+					<div className ="sloganContainer2">
+						<p>Plan.</p>
+						<p>Schedule.</p>
+						<p>HangOut.</p>
+					</div>
+					<div className="buttonContainer">
+						<button className="registerButton">REGISTER</button>
+						<button className="loginButton">LOGIN</button>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	);
 }
-
 export default Home;
